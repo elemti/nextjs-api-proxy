c170d6f24459af8d0da5ac6e7f3e9eb4aef85fe8
Thai <elemti.lmt@gmail.com>
2023-05-22 01:42:59 +0700

    added www.fruityvice.com

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+const proxyApis = [
+  {
+    source: "/www.fruityvice.com/:slug*",
+    destination: "https://www.fruityvice.com/:slug*",
+  },
+];
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+    return [...proxyApis];
+  },
+  headers: async () => {
+    // safe to use wildcard CORS for APIs not behind firewall
+    // https://stackoverflow.com/a/43154277
+    const corsHeaders = Object.entries({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+    }).map(([key, value]) => ({ key, value }));
+    return [
+      ...proxyApis.map(({ source }) => ({ source, headers: corsHeaders })),
+    ];
+  },
+};
+
+module.exports = nextConfig;
